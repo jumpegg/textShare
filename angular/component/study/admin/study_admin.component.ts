d0d4c1705631db3ae6fc4deb42d1928cc0d79beb
jumpegg <jumpegg@naver.com>
2017-05-12 20:07:56 +0900

    schedule view

--- angular/component/study/admin/study_admin.component.ts
+++ angular/component/study/admin/study_admin.component.ts
@@ -1,13 +1,19 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import { StudyPageInfo } from '../../../service/single_studypage';
 
 @Component({
-    styleUrls: ['client/component/study/admin/study_admin.component.css'],
-    templateUrl: 'client/component/study/admin/study_admin.component.html'
+		styleUrls: ['client/component/study/admin/study_admin.component.css'],
+		templateUrl: 'client/component/study/admin/study_admin.component.html'
 })
 export class StudyAdmin {
-    public title:string;
-    constructor(){
-        this.title = "this is study admin";
-    }
+		public title:string;
+		constructor(
+			public studyPage:StudyPageInfo
+		){
+				this.title = "this is study admin";
+		}
+		ngOnInit(){
+			this.studyPage.init();
+		}
 }
--- angular/component/study/schedule/study_schedule.component.ts
+++ angular/component/study/schedule/study_schedule.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { IMyOptions } from 'mydatepicker';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 import { StudyService } from '../../../service/study.service';
 import { StudyPageInfo } from '../../../service/single_studypage';
 
@@ -9,15 +9,37 @@ declare var naver : any;
 @Component({
 	styleUrls: ['client/component/study/schedule/study_schedule.component.css'],
 	templateUrl: 'client/component/study/schedule/study_schedule.component.html',
-	providers: [StudyService]
+	providers: [StudyService],
+	animations:[
+		trigger('tableToggle',[
+			state('open', style({})),
+			state('close', style({
+				height: '0px',
+				display: 'none'
+			})),
+			transition('open => close', animate('300ms ease-in')),
+			transition('close => open', animate('300ms ease-out'))
+		])
+	]
 })
 export class StudySchedule implements OnInit{
 	public title: string;
+	public map: any;
+	public tableState:string = 'close';
 
-	constructor(){
+	constructor(
+		public studyPage:StudyPageInfo
+	){
 		this.title = "this is study schedule";
 	}
 	ngOnInit(){
-
+		this.studyPage.init();
+			this.map = new naver.maps.Map('map', {
+				center: new naver.maps.LatLng(37.3595704, 127.105399),
+				zoom: 5
+			});
+	}
+	tableOpener(){
+		this.tableState = (this.tableState == 'close') ? 'open' : 'close';
 	}
 }
