0356e09b08e894881190b3e38c5ada006a6fd588
jumpegg <jumpegg@naver.com>
2017-05-22 01:00:02 +0900

    flow

--- angular/component/study/flow/study_flow.component.ts
+++ angular/component/study/flow/study_flow.component.ts
@@ -1,13 +1,33 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+import { StudyService } from '../../../service/study.service';
+import { StudyPageInfo } from '../../../service/single_studypage';
+
+import { Flow } from '../../../vo/flow';
+
+declare var $ : any;
 
 @Component({
-    styleUrls: ['client/component/study/flow/study_flow.component.css'],
-    templateUrl: 'client/component/study/flow/study_flow.component.html'
+		styleUrls: ['client/component/study/flow/study_flow.component.css'],
+		templateUrl: 'client/component/study/flow/study_flow.component.html',
+		providers: [StudyService]
 })
 export class StudyFlow {
-    public title:string;
-    constructor(){
-        this.title = "this is study flow";
-    }
+		public title:string;
+		public detailState:string = "close";
+		public newFlow:Flow = new Flow();
+		constructor(){
+				this.title = "this is study flow";
+		}
+		ngOnInit(){
+			$('.datepicker').pickadate({
+			selectMonths: true, // Creates a dropdown to control month
+			selectYears: 17, // Creates a dropdown of 15 years to control year
+			format: 'yyyy-mm-dd'
+		});
+		}
+		flowSubmit(input){
+			input.speak_date = $('.datepicker').val();
+		}
 }
--- angular/vo/flow.ts
+++ angular/vo/flow.ts
@@ -1,9 +1,12 @@
-export class Comment{
+export class Flow{
 	idx:number;
 	study_idx:number;
 	user_idx:number;
 	id:string;
-	comment:string;
+	speaker:string;
+	title:string;
+	content:string;
+	speak_date:Date;
 	c_date:Date;
 	constructor(){}
 }
